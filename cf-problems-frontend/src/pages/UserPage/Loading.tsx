089266c0840b7cd0a889c677f9c544a90a9f5864
kira924age <kira924age@gmail.com>
2021-03-06 02:11:39 +0900

    first commit

--- cf-problems-frontend/src/pages/UserPage/Loading.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Spin } from "antd";
-
-const Loading: React.FunctionComponent = (props) => {
-  return <Spin size="large" />;
-};
-
-export default Loading;
--- cf-problems-frontend/src/pages/UserPage/UserNotFound.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Alert } from "antd";
-
-const UserNotFound: React.FunctionComponent = (props) => {
-  return <Alert message="User not found!" type="error" />;
-};
-
-export default UserNotFound;

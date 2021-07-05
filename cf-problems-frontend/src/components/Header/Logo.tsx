ee1209ddaf482206ad96591e5237705308d4fba7
kira924age <kira924age@gmail.com>
2021-07-05 11:54:07 +0900

    Update Header

--- cf-problems-frontend/src/components/Header/Logo.tsx
+++ cf-problems-frontend/src/components/Header/Logo.tsx
@@ -3,14 +3,11 @@ import "./Logo.css";
 
 import { Link } from "react-router-dom";
 
-// src={process.env.PUBLIC_URL + "my_logo.png"
-// src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
 const Logo: React.FunctionComponent = (props) => {
   return (
     <h1 id="logo">
       <Link to="/table">
-        <img alt="logo" src="../CF-like-logo.png" />
-        Codeforces Problems{" "}
+        Codeforces Problems
       </Link>
     </h1>
   );

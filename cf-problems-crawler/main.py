8bc7d66db79df69f2ee0ac06a9666f2a75b19856
kira924age <kira924age@gmail.com>
2024-08-16 18:43:26 +0900

    ci: 🎡 use undetected-chromedriver for bypass cloudflare

--- cf-problems-crawler/main.py
+++ cf-problems-crawler/main.py
@@ -1,5 +1,6 @@
 import time
 
+import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,26 +11,33 @@ from utils import classify_contest_type
 
 
 def get_driver():
-    options = webdriver.ChromeOptions()
+    # options = webdriver.ChromeOptions()
+    # options.add_argument(
+    #     "--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0 Mobile/14C92 Safari/602.1"
+    # )
+    # options.add_argument("--headless")
+    # options.add_argument("start-maximized")
+    # options.add_argument("enable-automation")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--disable-infobars")
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--disable-browser-side-navigation")
+    # options.add_argument("--no-proxy-server")
 
+    # driver_path = ChromeDriverManager().install()
+    # driver = webdriver.Chrome(
+    #     service=Service(executable_path=driver_path), options=options
+    # )
+
+    options = uc.ChromeOptions()
+    options.headless = True
     options.add_argument(
         "--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0 Mobile/14C92 Safari/602.1"
     )
 
-    options.add_argument("--headless")
-    options.add_argument("start-maximized")
-    options.add_argument("enable-automation")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-browser-side-navigation")
-    options.add_argument("--no-proxy-server")
-
-    driver_path = ChromeDriverManager().install()
-    driver = webdriver.Chrome(
-        service=Service(executable_path=driver_path), options=options
-    )
+    # initialize the undetected Chrome driver with specified options
+    driver = uc.Chrome(use_subprocess=True, options=options)
     driver.implicitly_wait(10)
 
     return driver
